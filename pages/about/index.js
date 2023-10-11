660bae3672d63c54f1237530b092822b5e9a6d2d
Jacob Heinrich <heinrija@oregonstate.edu>
2023-10-10 22:06:14 -0600

    Added new logo and changed formatting
    - Added logo
    - Fixed design
    - Changed About Me page

--- pages/about/index.js
+++ pages/about/index.js
@@ -4,77 +4,27 @@ import { useState } from "react";
 import { Fragment} from 'react';
 import { Listbox, Transition } from '@headlessui/react';
 import { CheckIcon, ChevronUpDownIcon } from '@heroicons/react/20/solid';
-
-const links = [
-    { name: 'Email', href: '#' },
-    { name: 'Internship program', href: '#' },
-    { name: 'Our values', href: '#' },
-    { name: 'Meet our leadership', href: '#' },
-  ]
-  const stats = [
-    { name: 'Offices worldwide', value: '12' },
-    { name: 'Full-time colleagues', value: '300+' },
-    { name: 'Hours per week', value: '40' },
-    { name: 'Paid time off', value: 'Unlimited' },
-  ]
-
-  const people = [
-    {
-      id: 1,
-      name: 'Software Engineering 1'
-    },
-    {
-      id: 2,
-      name: 'Software Engineering 2'
-    },
-    {
-      id: 3,
-      name: 'Web Developement'
-    },
-    {
-      id: 4,
-      name: 'Intro to Computer Graphics'
-    },
-    {
-      id: 5,
-      name: 'Discrete Mathematics'
-    },
-    {
-      id: 6,
-      name: 'Operating Systems'
-    },
-    {
-      id: 7,
-      name: 'Assembly Language'
-    },
-    {
-      id: 8,
-      name: 'Data Structures'
-    },
-  ]
-  
-
-  function classNames(...classes) {
-    return classes.filter(Boolean).join(' ')
-  }
+import jake_wave from "../../public/jacob_w.png";
+import Image from "next/image";
+import motion from '../../public/motionelements.gif';
+import {FaCameraRetro} from 'react-icons/fa';
+import {AiFillHome} from 'react-icons/Ai';
 
 export default function AboutPage() {
     const [darkMode, setDarkMode] = useState(true);
-    const [selected, setSelected] = useState(people[3])
 
     return (
         <div className={darkMode ? "dark" : ""}>
-        <main className=" bg-white px-10 dark:bg-gray-900 md:px-20 lg:px-40">
+        <main className="bg-white dark:bg-gray-900">
         <section>
-          <nav className="py-10 mb-12 flex justify-between dark:text-white">
-            <h1 className="font-burtons text-xl">developedbyJacob</h1>
+        <nav className="py-4 mb-12 flex justify-between dark:text-white">
+            <div>
+            <h1 className="font-burtons text-xl pl-10">developedbyJacob</h1>
+            </div>
+            <div className="hidden lg:flex pr-10">
             <ul className="flex items-center">
-            <Link href="/">
-                <button className="bg-cyan-500 hover:bg-cyan-600 text-white px-4 py-2 border-none rounded-md ml-8">Home</button>
-                </Link>
-                <Link href="/photography">
-                <button className="bg-cyan-500 hover:bg-cyan-600 text-white px-4 py-2 border-none rounded-md ml-8">Photography</button>
-                </Link>
+            <Link href="/"><button className="bg-cyan-500 hover:bg-cyan-600 text-white px-4 py-2 border-none rounded-md ml-8">Home</button></Link>
+            <Link href="/photography"><button className="bg-cyan-500 hover:bg-cyan-600 text-white px-4 py-2 border-none rounded-md ml-8">Photography</button></Link>
               <li className="pl-8">
                 <BsFillMoonStarsFill
                   onClick={() => setDarkMode(!darkMode)}
@@ -82,119 +32,65 @@ export default function AboutPage() {
                 />
               </li>
             </ul>
-          </nav>
-          <div className="relative isolate overflow-hidden py-24 sm:py-32">
-      <div
-        className="hidden sm:absolute sm:-top-10 sm:right-1/2 sm:-z-10 sm:mr-10 sm:block sm:transform-gpu sm:blur-3xl"
-        aria-hidden="true"
-      >
-      </div>
-      <div className="mx-auto max-w-7xl px-6 lg:px-8">
-        <div className="mx-auto max-w-2xl lg:mx-0">
-          <h2 className="text-4xl font-bold tracking-tight text-black dark:text-white sm:text-6xl">Hello!</h2>
-          <p className="mt-6 text-lg leading-8 text-black dark:text-white">
-            I am Jacob Heinrich. After spending 8 years in the military I decided to pursue my love 
-            for technology. This lead me pursue a B.S. in Computer Science from Colorado 
-            State University and have had the opportunity to start working at Geek Squad in 2022 while I finish my degree.
-          </p>
-        </div>
-        <div className="mx-auto mt-10 max-w-2xl lg:mx-0 lg:max-w-none">
-          <div className="grid grid-cols-3 gap-x-8 gap-y-6 text-base font-semibold leading-7 text-black dark:text-white sm:grid-cols-2 md:flex lg:gap-x-10">
-            <div>
-                <h1>Email: jacobheinrich45@gmail.com</h1>
             </div>
-            <div>
-                <h1>Github: <a href="https://github.com/jacohein" target="_blank" className="hover:text-cyan-300" >jacohein</a></h1>
-            </div>
-            <div>
-            <Listbox value={selected} onChange={setSelected}>
-      {({ open }) => (
-        <>
-          <Listbox.Label className="mx-auto mt-10 max-w-2xl lg:mx-0 lg:max-w-none">Courses Taken</Listbox.Label>
-          <div className="relative mt-2">
-            <Listbox.Button className="relative w-full cursor-default rounded-md bg-white py-1.5 pl-3 pr-10 text-left text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:outline-none focus:ring-2 focus:ring-indigo-500 sm:text-sm sm:leading-6">
-              <span className="flex items-center">
-                <span className="ml-3 block truncate">{selected.name}</span>
-              </span>
-              <span className="pointer-events-none absolute inset-y-0 right-0 ml-3 flex items-center pr-2">
-                <ChevronUpDownIcon className="h-5 w-5 text-gray-400" aria-hidden="true" />
-              </span>
-            </Listbox.Button>
-
-            <Transition
-              show={open}
-              as={Fragment}
-              leave="transition ease-in duration-100"
-              leaveFrom="opacity-100"
-              leaveTo="opacity-0"
+            <div className="dark:bg-gray-900 flex items-center justify-between lg:hidden">
+      <div className="">
+        <div>
+          <nav className="isolate inline-flex -space-x-px rounded-md shadow-sm" aria-label="Pagination">
+            <Link
+              href="/"
+              aria-current="page"
+              className="relative z-10 inline-flex items-center bg-indigo-600 px-4 py-2 text-sm font-semibold text-white focus:z-20 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+            > 
+              <button className="text-white pr-10 text-2xl"><AiFillHome/></button>
+            </Link>
+            <Link
+              href="/photography"
+              className="relative inline-flex items-center px-4 py-2 text-sm font-semibold text-gray-900 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-20 focus:outline-offset-0"
             >
-              <Listbox.Options className="absolute z-10 mt-1 max-h-56 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm">
-                {people.map((person) => (
-                  <Listbox.Option
-                    key={person.id}
-                    className={({ active }) =>
-                      classNames(
-                        active ? 'bg-indigo-600 text-white' : 'text-gray-900',
-                        'relative cursor-default select-none py-2 pl-3 pr-9'
-                      )
-                    }
-                    value={person}
-                  >
-                    {({ selected, active }) => (
-                      <>
-                        <div className="flex items-center">
-                          <span
-                            className={classNames(selected ? 'font-semibold' : 'font-normal', 'ml-3 block truncate')}
-                          >
-                            {person.name}
-                          </span>
-                        </div>
+              <button className="text-white pr-10 text-2xl"><FaCameraRetro/></button>
+            </Link>
+          </nav>
+        </div>
+      </div>
+    </div>
+          </nav>
+          <div className="xl:h-screen xl:grid gap-5 grid-cols-4 grid-rows-4 pl-72">
+            <div className="mx-auto bg-gradient-to-b from-teal-500 rounded-full w-80 h-80 relative overflow-hidden mt-20 md:h-96 md:w-96 row-start-1 row-end-3 col-start-2 col-end-3"><Image src={jake_wave}/></div>
+            <div className="row-start-0 row-end-0 col-start-1 col-start-2 pl-10">
+              <h1 className="text-center text-white text-4xl font-bold decoration-solid underline font-burtons pt-10 xl:pt-0">Bio</h1>
+              <p className="text-white pt-4">
+                After spending 8 years as an Aircraft Armament Technician in the Air Force I decided to pursue a career in the technology field. From attending
+                Colorado State University for a B.S. in Computer Science I found my passion for solving complex problems through coding. 
+              </p>
+              <div className="text-white pt-4">
+              Along with the technical skills I learned 
+              through projects and school work, I have been fortunate to gain experience in:
+              <ul className="list-disc pl-4">
+                <li>Adaptibility</li>
+                <li>Communication</li>
+                <li>Problem Solving</li>
+                <li>Leadership</li>
+              </ul>
 
-                        {selected ? (
-                          <span
-                            className={classNames(
-                              active ? 'text-white' : 'text-indigo-600',
-                              'absolute inset-y-0 right-0 flex items-center pr-4'
-                            )}
-                          >
-                            <CheckIcon className="h-5 w-5" aria-hidden="true" />
-                          </span>
-                        ) : null}
-                      </>
-                    )}
-                  </Listbox.Option>
-                ))}
-              </Listbox.Options>
-            </Transition>
-          </div>
-        </>
-      )}
-    </Listbox>
-            </div>
-          </div>
-          <div className="pt-4 text-center text-white dark:text-black">
-          {
-            selected.id === 1 ? <div className="p-1.5 rounded-lg bg-gray-900 dark:bg-white w-1/2 min-h-fit"><h1 className="font-bold">Microservice Implementation</h1><p className="font-thin">For this project I was tasked to write a microservice to handle email communications for my group member's project. I wrote this service in Python using the packages email and smtplib. </p></div> : 
-            selected.id === 2 ? <div className="p-1.5 rounded-lg bg-gray-900 dark:bg-white w-1/2 min-h-fit"><h1 className="font-bold">Unit Testing <p className="font-thin">During this class I learned more about unit testing. This includes Black Box Testing, White Box Testing, Random Testing, Test Driven Development, Continuous Integration, and doing code reviews with group members.</p></h1></div> : 
-            selected.id === 3 ? <div className="p-1.5 rounded-lg bg-gray-900 dark:bg-white w-1/2 min-h-fit"><h1 className="font-bold">Full Stack MERN Application<p className="font-thin">Had to build a web application using MondoDB for the database, Express.js to build an API to communicate, React to build the front end, and Node.js to run. </p></h1></div> :
-            selected.id === 4 ? <div className="p-1.5 rounded-lg bg-gray-900 dark:bg-white w-1/2 min-h-fit"><h1 className="font-bold">Intro to Computer Graphics</h1></div> :
-            selected.id === 5 ? <div className="p-1.5 rounded-lg bg-gray-900 dark:bg-white w-1/2 min-h-fit"><h1 className="font-bold">Discrete Mathematics</h1></div> :
-            selected.id === 6 ? <div className="p-1.5 rounded-lg bg-gray-900 dark:bg-white w-1/2 min-h-fit"><h1 className="font-bold">Operating Systems</h1></div> :
-            selected.id === 7 ? <div className="p-1.5 rounded-lg bg-gray-900 dark:bg-white w-1/2 min-h-fit"><h1 className="font-bold">Assembly Language</h1></div> :
-            <h1>Data Structures</h1> 
-          }
-          </div>
-          <dl className="mt-16 grid grid-cols-1 gap-8 sm:mt-20 sm:grid-cols-2 lg:grid-cols-4">
-            {stats.map((stat) => (
-              <div key={stat.name} className="flex flex-col-reverse">
-                <dt className="text-base leading-7 text-black">{stat.name}</dt>
-                <dd className="text-2xl font-bold leading-9 tracking-tight text-black">{stat.value}</dd>
               </div>
-            ))}
-                    </dl>
-                    </div>
-                    </div>
-                </div>
+              </div>
+            <div className="row-start-0 row-end-0 col-start-3 cold-end-3">
+              <h1 className="text-white text-4xl font-bold decoration-solid underline font-burtons pt-24 text-center xl:text-left">Hobbies</h1>
+              <p className="text-white pt-4 text-center xl:text-left pl-6">Skiing, Photography, Cooking, Coding</p>
+            </div>
+            <div className="row-start-2 row-end-2 col-start-3 col-end-3">
+              <h1 className="text-center text-white decoration-solid underline text-4xl font-bold font-burtons pt-24 xl:pl-8 xl:text-left">Education</h1>
+              <p className="text-center text-white pt-4 xl:text-left xl:pl-6">Colorado State University</p>
+              <p className="text-white text-center italic xl:text-left xl:pl-6">B.S. in Computer Science</p>
+              <p className="text-center text-white pt-8 xl:text-left xl:pl-6">University of Colorado</p>
+              <p className="text-white text-center italic xl:text-left xl:pl-6">B.A. in Economics</p>
+            </div>
+            <div className="row-start-2 row-end-2 col-start-1 col-end-3">
+              <h1 className="text-center text-white decoration-solid underline text-4xl font-bold font-burtons pt-24 xl:pt-60 xl:pr-36">Technology Strengths</h1>
+              <p className="text-white xl:pl-16 pt-4 pb-10">Python, HTML/CSS, Javascript, React, Next.js, TailwindCSS</p>
+            </div>
+            </div>
             </section>
         </main>
     </div>
--- pages/photography/index.js
+++ pages/photography/index.js
@@ -7,6 +7,9 @@ import flower from "../../public/flower.jpeg";
 import sanfran from "../../public/sanfran.jpeg";
 import ladybug from "../../public/ladybug.jpeg";
 import Image from "next/image";
+import jake_wave from "../../public/jacob_w.png";
+import {AiFillHome} from 'react-icons/Ai';
+import {BsPersonCircle} from 'react-icons/bs';
 
 export default function PhotographyPage() {
     const [darkMode, setDarkMode] = useState(true);
@@ -15,12 +18,14 @@ export default function PhotographyPage() {
         <div className={darkMode ? "dark" : ""}>
         <main className=" bg-white px-10 dark:bg-gray-900 md:px-20 lg:px-40">
         <section>
-          <nav className="py-10 mb-12 flex justify-between dark:text-white">
+        <nav className="py-4 mb-12 flex justify-between dark:text-white">
+            <div>
             <h1 className="font-burtons text-xl">developedbyJacob</h1>
+            </div>
+            <div className="hidden lg:flex">
             <ul className="flex items-center">
-            <Link href="/">
-                <button className="bg-cyan-500 hover:bg-cyan-600 text-white px-4 py-2 border-none rounded-md ml-8">Home</button>
-                </Link>
+            <Link href="/"><button className="bg-cyan-500 hover:bg-cyan-600 text-white px-4 py-2 border-none rounded-md ml-8">Home</button></Link>
+            <Link href="/about"><button className="bg-cyan-500 hover:bg-cyan-600 text-white px-4 py-2 border-none rounded-md ml-8">About</button></Link>
               <li className="pl-8">
                 <BsFillMoonStarsFill
                   onClick={() => setDarkMode(!darkMode)}
@@ -28,6 +33,28 @@ export default function PhotographyPage() {
                 />
               </li>
             </ul>
+            </div>
+            <div className="dark:bg-gray-900 flex items-center justify-between lg:hidden">
+      <div className="">
+        <div>
+          <nav className="isolate inline-flex -space-x-px rounded-md shadow-sm" aria-label="Pagination">
+            <Link
+              href="/"
+              aria-current="page"
+              className="relative z-10 inline-flex items-center bg-indigo-600 px-4 py-2 text-sm font-semibold text-white focus:z-20 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+            > 
+              <button className="text-white pr-10 text-2xl"><AiFillHome/></button>
+            </Link>
+            <Link
+              href="/about"
+              className="relative inline-flex items-center px-4 py-2 text-sm font-semibold text-gray-900 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-20 focus:outline-offset-0"
+            >
+              <button className="text-white pr-10 text-2xl"><BsPersonCircle/></button>
+            </Link>
+          </nav>
+        </div>
+      </div>
+    </div>
           </nav>
           </section>
           <div className="flex flex-col gap-10 py-10 lg:flex-row lg:flex-wrap">
