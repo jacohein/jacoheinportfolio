eacc2af8c615b4166800a6d5f339afca6bdaa398
jacohein <jacobheinrich45@gmail.com>
2023-11-16 13:14:54 -0700

    update index.js

--- pages/index.js
+++ pages/index.js
@@ -44,9 +44,9 @@ export default function Home() {
   }
 
   const items = [
-  { id: 1, title: "Back End Developer", department: "Engineering", type: "Full-time", location: "Remote" },
-  { id: 2, title: "Front End Developer", department: "Engineering", type: "Full-time", location: "Remote" },
-  { id: 3, title: "User Interface Designer", department: "Design", type: "Full-time", location: "Remote" },
+  { id: 1, title: 'Back End Developer', department: 'Engineering', type: 'Full-time', location: 'Remote' },
+  { id: 2, title: 'Front End Developer', department: 'Engineering', type: 'Full-time', location: 'Remote' },
+  { id: 3, title: 'User Interface Designer', department: 'Design', type: 'Full-time', location: 'Remote' },
   ]
 
   useEffect(() => {
